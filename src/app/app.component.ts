cb7cc550668443750af91f5a6cca269cb17b8fe8
Alexandre Almeida <asoares73@gmail.com>
2018-12-31 00:16:53 -0200

    Adicionando informações do usuario logado no menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
+import { ClienteDTO } from '../models/cliente.dto';
+import { API_CONFIG } from '../config/api.config';
 
 @Component({
   selector : 'myapp',
   templateUrl: 'app.html'
 })
 export class MyApp {
+  cliente : ClienteDTO;
+
   @ViewChild(Nav) nav: Nav;
 
   rootPage: string = 'HomePage';
@@ -19,7 +25,9 @@ export class MyApp {
     public platform: Platform, 
     public statusBar: StatusBar, 
     public splashScreen: SplashScreen,
-    public auth: AuthService) {
+    public auth: AuthService,
+    public storage: StorageService,
+    public clienteService: ClienteService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -38,6 +46,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.carrega();
     });
   }
 
@@ -51,5 +61,26 @@ export class MyApp {
       default: 
       this.nav.setRoot(page.component);
     }
+  }
+
+  carrega() {
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExists();
+        },
+        error => {});
+    }
+
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+        error => {});
   }
 }
