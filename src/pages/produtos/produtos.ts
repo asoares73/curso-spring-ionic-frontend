f77effd83c6bc5a8a495081b7e5c26367c9252e8
Alexandre Almeida <asoares73@gmail.com>
2019-01-02 12:06:09 -0200

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();    
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -29,24 +33,24 @@ export class ProdutosPage {
         loader.dismiss();
         this.loadImageUrls();
       },
-        error => { 
+        error => {
           loader.dismiss();
         });
   }
 
   loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+    for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(Response => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
         },
-        error => {});
+          error => { });
     }
   }
 
-  showDetail(produto_id : string) {
-    this.navCtrl.push('ProdutoDetailPage',{produto_id: produto_id});
+  showDetail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
   }
 
   presentLoading() {
@@ -55,5 +59,12 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();    
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
